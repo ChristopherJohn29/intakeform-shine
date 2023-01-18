24756403b91ce68bf3bd21277564c6024c41ef4a
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-01-19 00:13:47 +0800

    Authetication

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://139.144.191.120/';
+$config['base_url'] = 'http://appointment.shinemmg.com/';
 
 /*
 |--------------------------------------------------------------------------
