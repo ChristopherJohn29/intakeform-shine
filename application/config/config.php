4b212249b5c1c7b61a2f9a97d1f4909d5b0cdca7
Christopher John Gamo <christopherjohngamo@gmail.com>
2023-02-07 02:44:42 +0800

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://appointment.shinemmg.com/';
+$config['base_url'] = 'https://appointment.shinemmg.com/';
 
 /*
 |--------------------------------------------------------------------------
